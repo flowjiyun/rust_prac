8ed5165e70b408bba571d8ee74bd4fe39c2c215c
flowjiyun <gyp2795@gmail.com>
2024-05-04 21:11:31 +0900

    feat : guess game 완성

--- guess_game/src/main.rs
+++ guess_game/src/main.rs
@@ -1,11 +1,30 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("Input your guess!");
+    let secret: u32 = rand::thread_rng().gen_range(1..=100);
 
-    let mut guess = String::new();
+    loop {
+        println!("Input your guess!");
+        let mut guess: String = String::new();
 
-    io::stdin().read_line(&mut guess).expect("readline error");
+        io::stdin().read_line(&mut guess).expect("readline error");
 
-    println!("your guess is {guess}");
+        println!("your guess is {}", guess.trim());
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("Too Small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+              println!("You Win!");
+              break;
+            },
+        }
+    }
 }
